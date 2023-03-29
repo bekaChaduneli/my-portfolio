aacbb3d03cb05e6957cdd147482bd50654a57c04
beka chaduneli <beka@kali.ge>
2023-03-29 20:49:03 +0400

    Testing bugs

--- src/components/MainLogo.tsx
+++ src/components/MainLogo.tsx
@@ -1,15 +1,14 @@
-import { Box } from "@chakra-ui/react";
 import { OrbitControls, Stage } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Avatar } from "./Avatar";
-import CanvasLoader from "./Loader";
 
+import CanvasLoader from "./Loader";
 export default function MainLogo() {
   return (
     <Canvas>
       <Suspense fallback={<CanvasLoader />}>
-        <Stage environment="night" intensity={0.9}>
+        <Stage environment="night" intensity={1}>
           <Avatar />
         </Stage>
         <OrbitControls enableZoom={false} />
