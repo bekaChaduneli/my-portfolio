9f3b821e2c66765f885746343d2821d536cb9884
beka chaduneli <beka@kali.ge>
2023-03-30 20:31:45 +0400

    Add animations

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,5 +1,12 @@
+import { Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <Flex w="100%" h="80vh" justifyContent="center" alignItems="center">
+      <Text fontSize="70px" color="black" fontWeight="900">
+        Will Be Soon
+      </Text>
+    </Flex>
+  );
 }
--- src/components/AllSkills.tsx
+++ src/components/AllSkills.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
-
+import { motion } from "framer-motion";
 export default function AllSkills(props: any) {
   return (
     <Flex
@@ -38,61 +38,72 @@ export default function AllSkills(props: any) {
       >
         {props.data.skills.map((skill: any) => {
           return (
-            <Flex
-              _hover={{
-                mt: "-8px",
-                transition: "0.2s",
-                boxShadow: "md",
+            <motion.div
+              className="box"
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
               }}
-              borderRadius="7px"
-              boxShadow="sm"
-              flexDir="column"
-              color="brand.100"
-              w={{ sm: "147px", md: "235px", lg: "300px", xl: "350px" }}
-              h={{ sm: "110px", md: "175px", lg: "210px", xl: "270px" }}
             >
               <Flex
-                justifyContent="center"
-                alignItems="Center"
-                w="100%"
-                h={{ sm: "70px", md: "125px", lg: "145px", xl: "200px" }}
-                bg="brand.500"
+                _hover={{
+                  mt: "-8px",
+                  transition: "0.2s",
+                  boxShadow: "md",
+                }}
+                borderRadius="7px"
+                boxShadow="sm"
+                flexDir="column"
+                color="brand.100"
+                w={{ sm: "147px", md: "235px", lg: "300px", xl: "350px" }}
+                h={{ sm: "110px", md: "175px", lg: "210px", xl: "270px" }}
               >
-                <Flex justifyContent="center" alignItems="center" gap="15px">
-                  <Image
-                    borderTopRadius="7px"
-                    maxH={{ sm: "20px", md: "30px", lg: "24px", xl: "40px" }}
-                    maxW={{ sm: "30px", md: "40px", lg: "50px", xl: "60px" }}
-                    src={skill.icon}
-                  />
+                <Flex
+                  justifyContent="center"
+                  alignItems="Center"
+                  w="100%"
+                  h={{ sm: "70px", md: "125px", lg: "145px", xl: "200px" }}
+                  bg="brand.500"
+                >
+                  <Flex justifyContent="center" alignItems="center" gap="15px">
+                    <Image
+                      borderTopRadius="7px"
+                      maxH={{ sm: "20px", md: "30px", lg: "24px", xl: "40px" }}
+                      maxW={{ sm: "30px", md: "40px", lg: "50px", xl: "60px" }}
+                      src={skill.icon}
+                    />
+                    <Text
+                      fontWeight="700"
+                      fontSize={{ sm: " 12px", md: "20px", xl: "24px" }}
+                    >
+                      {skill.name}
+                    </Text>
+                  </Flex>
+                </Flex>
+                <Flex
+                  mt={{ sm: "12px", md: "15px", lg: "17px", xl: "23px" }}
+                  fontWeight="500"
+                  justifySelf="center"
+                  gap="6px"
+                  color="black"
+                  alignSelf="center"
+                >
+                  <Text fontSize={{ sm: "10px", md: "12px", lg: "16px" }}>
+                    Projects Number :
+                  </Text>
                   <Text
+                    fontSize={{ sm: "10px", md: "12px", lg: "16px" }}
                     fontWeight="700"
-                    fontSize={{ sm: " 12px", md: "20px", xl: "24px" }}
                   >
-                    {skill.name}
+                    {" "}
+                    {skill.projectsnumber}
                   </Text>
                 </Flex>
               </Flex>
-              <Flex
-                mt={{ sm: "12px", md: "15px", lg: "17px", xl: "23px" }}
-                fontWeight="500"
-                justifySelf="center"
-                gap="6px"
-                color="black"
-                alignSelf="center"
-              >
-                <Text fontSize={{ sm: "10px", md: "12px", lg: "16px" }}>
-                  Projects Number :
-                </Text>
-                <Text
-                  fontSize={{ sm: "10px", md: "12px", lg: "16px" }}
-                  fontWeight="700"
-                >
-                  {" "}
-                  {skill.projectsnumber}
-                </Text>
-              </Flex>
-            </Flex>
+            </motion.div>
           );
         })}
       </Flex>
